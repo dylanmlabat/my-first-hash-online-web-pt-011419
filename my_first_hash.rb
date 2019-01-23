125f1949566ad081dd74caccd0e8ced295e3ce94
Dylan Michael Labat <dml252@live.com>
2019-01-23 01:39:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {}
+  my_hash = {"a" => 1, "b" => 2, "c" => 3}
   
 end
 
@@ -22,7 +22,6 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
 end
